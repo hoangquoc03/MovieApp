fe4ed8dad3ed9f0d585d8ef6312ee59e02340bc0
lequoc <lequoc150305@quoc150305gmail.com>
2025-11-14 14:03:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import Header from "./components/Header.jsx";
+import Banner from "./components/Banner.jsx";
+import Movielist from "./components/MovieList.jsx";
+import { useState, useEffect } from "react";
+import MovieSearch from "./components/MovieSearch.jsx";
+import { MovieProvider } from "./context/MovieProvider.jsx";
+function App() {
+  const [movie, setMovie] = useState([]);
+  const [movieRate, setMovieRate] = useState([]);
+  const [movieSearch, setMovieSearch] = useState([]);
+  const handleSearch = async (searchVal) => {
+    setMovieSearch([]);
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?query=${searchVal}%20&include_adult=false&language=en-vi&page=1`;
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+      };
+      const searchMovie = await fetch(url, options);
+      const data = await searchMovie.json();
+      setMovieSearch(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+      };
+      const url1 =
+        "https://api.themoviedb.org/3/movie/popular?language=en-vi&page=1";
+
+      const url2 =
+        "https://api.themoviedb.org/3/movie/top_rated?language=en-vi&page=1";
+      const [res1, res2] = await Promise.all([
+        fetch(url1, options),
+        fetch(url2, options),
+      ]);
+      const data1 = await res1.json();
+      const data2 = await res2.json();
+      setMovie(data1.results);
+      setMovieRate(data2.results);
+    };
+    fetchMovie();
+  }, []);
+  return (
+    <>
+      <MovieProvider>
+        <div className="bg-black pb-10">
+          <Header onSearch={handleSearch}></Header>
+          <Banner></Banner>
+          {movieSearch.length > 0 ? (
+            <MovieSearch title={"Kết quả tìm kiếm"} data={movieSearch} />
+          ) : (
+            <>
+              <Movielist title={"Phim hot"} data={movie}></Movielist>
+              <Movielist title={"Phim Đề Cử"} data={movieRate}></Movielist>
+            </>
+          )}
+        </div>
+      </MovieProvider>
+    </>
+  );
+}
+
+export default App;
